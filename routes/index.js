dc54e7a3c19122133c62a40cdaa814a8e7e19fb2
Kazuaki Nishizawa <kazuaki_nishizawa@hotmail.com>
2012-09-12 23:17:43 +0900

    add date and time at index page

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,25 @@
  * GET home page.
  */
 
+var date1 = DateString(new Date());
+
+exports.date1 = date1;
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Node Express Hello World 1' });
+  res.render('index', {
+    title: 'Node Express Hello World 1',
+    date1: date1,
+    date2: DateString(new Date())
+  });
 };
+
+// 日付
+function DateString(d) {
+  function pad(n){return n<10 ? '0'+n : n}  
+  return d.getFullYear()+'-'  
+      + pad(d.getMonth()+1)+'-'  
+      + pad(d.getDate())+' '  
+      + pad(d.getHours())+':'  
+      + pad(d.getMinutes())+':'  
+      + pad(d.getSeconds())  
+}  
