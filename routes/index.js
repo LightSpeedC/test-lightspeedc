aa235b233222c0f9c24e13b2e628c7ae30c059bb
Kazuaki Nishizawa <kazuaki_nishizawa@hotmail.com>
2012-09-12 23:27:34 +0900

    add ISO date and time, trim spaces at end of line

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@
  * GET home page.
  */
 
-var date1 = DateString(new Date());
+var date1 = DateString(new Date()) + ' (ISO: ' + ISODateString(new Date()) + ')';
 
 exports.date1 = date1;
 
@@ -11,17 +11,28 @@ exports.index = function(req, res){
   res.render('index', {
     title: 'Node Express Hello World 1',
     date1: date1,
-    date2: DateString(new Date())
+    date2: DateString(new Date()) + ' (ISO: ' + ISODateString(new Date()) + ')'
   });
 };
 
 // 日付
 function DateString(d) {
-  function pad(n){return n<10 ? '0'+n : n}  
-  return d.getFullYear()+'-'  
-      + pad(d.getMonth()+1)+'-'  
-      + pad(d.getDate())+' '  
-      + pad(d.getHours())+':'  
-      + pad(d.getMinutes())+':'  
-      + pad(d.getSeconds())  
-}  
+  function pad(n){return n<10 ? '0'+n : n}
+  return d.getFullYear()+'-'
+      + pad(d.getMonth()+1)+'-'
+      + pad(d.getDate())+' '
+      + pad(d.getHours())+':'
+      + pad(d.getMinutes())+':'
+      + pad(d.getSeconds())
+}
+
+// ISO日付
+function ISODateString(d) {
+  function pad(n){return n<10 ? '0'+n : n}
+  return d.getUTCFullYear()+'-'
+      + pad(d.getUTCMonth()+1)+'-'
+      + pad(d.getUTCDate())+'T'
+      + pad(d.getUTCHours())+':'
+      + pad(d.getUTCMinutes())+':'
+      + pad(d.getUTCSeconds())+'Z'
+}
