03c85a973413b15f1abe803a7d97154c39bdddac
Kazuaki Nishizawa <kazuaki_nishizawa@hotmail.com>
2012-09-13 00:31:32 +0900

    support timezone for Windows and Linux

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,29 @@
  * GET home page.
  */
 
-var time = require('time');
+if (process.platform != 'win32')
+  var time = require('time');
 
-//var date1 = DateString(new Date()) + ' (ISO: ' + ISODateString(new Date()) + ')';
-var date1 = new time.Date();
-date1.setTimezone('Asia/Tokyo');
-date1 = DateString(date1) + ' (ISO: ' + ISODateString(date1) + ')';
+if (process.platform == 'win32') {
+  var date1 = DateString(new Date()) + ' (ISO: ' + ISODateString(new Date()) + ')';
+}
+else {
+  var date1 = new time.Date();
+  date1.setTimezone('Asia/Tokyo');
+  date1 = DateString(date1) + ' (ISO: ' + ISODateString(date1) + ')';
+}
 
 exports.date1 = date1;
 
 exports.index = function(req, res){
-  //var date2 = DateString(new Date()) + ' (ISO: ' + ISODateString(new Date()) + ')';
-  var date2 = new time.Date();
-  date2.setTimezone('Asia/Tokyo');
-  date2 = DateString(date2) + ' (ISO: ' + ISODateString(date2) + ')';
+  if (process.platform == 'win32') {
+    var date2 = DateString(new Date()) + ' (ISO: ' + ISODateString(new Date()) + ')';
+  }
+  else {
+    var date2 = new time.Date();
+    date2.setTimezone('Asia/Tokyo');
+    date2 = DateString(date2) + ' (ISO: ' + ISODateString(date2) + ')';
+  }
 
   res.render('index', {
     title: 'Node Express Hello World 1',
